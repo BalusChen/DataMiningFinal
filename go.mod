239f5704f400b2aa3494f396e3e33e4e28d83243
chenjianyong <chenjianyong@bytedance.com>
2019-12-26 21:13:14 +0800

    Update: draw an image according to HasWifi field.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/BalusChen/DataMiningFinalProject
 
 go 1.12
+
+require (
+	gonum.org/v1/netlib v0.0.0-20191031114514-eccb95939662 // indirect
+	gonum.org/v1/plot v0.0.0-20191107103940-ca91d9d40d0a
+)
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"github.com/BalusChen/DataMiningFinalProject/classify"
+	"github.com/BalusChen/DataMiningFinalProject/csvutil"
+	"github.com/BalusChen/DataMiningFinalProject/draw"
+)
+
+const (
+	defaultTrainCSVFilename = "data/train.csv"
+)
+
+var (
+	trainCSVFilename string
+)
+
+func init() {
+	flag.StringVar(&trainCSVFilename, "t", defaultTrainCSVFilename, "train csv")
+}
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+
+	records := csvutil.ReadRecord(trainCSVFilename)
+	results, kindsOfOptions, name := classify.Wifi(records)
+	draw.BarChart(results, kindsOfOptions, name)
+}
+
+func drawDiscrete() {
+
+}
+
+func drawContinous() {
+
 }
